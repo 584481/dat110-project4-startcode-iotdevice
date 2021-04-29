b9d1853e849f84012da7fa5b8304fe408dd50325
h576069 <AnneDrottning@158-37-234-21.hvl.no>
2021-04-29 10:09:50 +0200

    gyuihjl

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -8,7 +8,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-
 public class RestClient {
 
 	public RestClient() {
@@ -20,29 +19,25 @@ public class RestClient {
 	public void doPostAccessEntry(String message) {
 
 		// TODO: implement a HTTP POST on the service to post the message
-		
+
 	}
-	
+
 	private static String codepath = "/accessdevice/code";
-	
+
 	public AccessCode doGetAccessCode() {
-       Gson gson = new Gson();
+		Gson gson = new Gson();
 		AccessCode code = null;
-		OkHttpClient client= new OkHttpClient();
-		Request request = new Request.Builder()
-				.url("http://localhost:8080"+codepath)
-				.get()
-				.build();
-		try(Response response = client.newCall(request).execute()){
-code = gson.fromJson(response.body(), Accesscode.class);
-			
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder().url("http://localhost:8080" + codepath).get().build();
+		try (Response response = client.newCall(request).execute()) {
+			code = gson.fromJson(gson.toJson(response.body().string()), AccessCode.class);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("oh no");
 			e.printStackTrace();
 		}
-		
+
 		// TODO: implement a HTTP GET on the service to get current access code
-		
+
 		return code;
 	}
 }
