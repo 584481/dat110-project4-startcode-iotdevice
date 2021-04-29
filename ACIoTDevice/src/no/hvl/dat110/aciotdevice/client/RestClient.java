6fe085861cc11c2a93ee859fa24377c9d570a2c3
h585013 <54100991+h585013@users.noreply.github.com>
2021-04-29 10:01:24 +0200

    asdfgh

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,5 +1,14 @@
 package no.hvl.dat110.aciotdevice.client;
 
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
 public class RestClient {
 
 	public RestClient() {
@@ -17,8 +26,20 @@ public class RestClient {
 	private static String codepath = "/accessdevice/code";
 	
 	public AccessCode doGetAccessCode() {
-
+       Gson gson = new Gson();
 		AccessCode code = null;
+		OkHttpClient client= new OkHttpClient();
+		Request request = new Request.Builder()
+				.url("http://localhost:8080"+codepath)
+				.get()
+				.build();
+		try(Response response = client.newCall(request).execute()){
+code = gson.fromJson(response.body(), Accesscode.class);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		// TODO: implement a HTTP GET on the service to get current access code
 		
